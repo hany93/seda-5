7e98290a31e64f7d6f0f7aae7207dfb62f23f88b
hany93 <hany93cuba@gmail.com>
2019-10-25 09:59:17 -0400

    cambios

--- src/variables/maps.js
+++ src/variables/maps.js
@@ -7,62 +7,53 @@ import { defaults } from 'ol/interaction';
 import { defaults as defaultsControls, FullScreen } from 'ol/control';
 import nuevoazul from 'assets/img/nuevoazul.png'
 import Point from 'ol/geom/Point';
-import { Style, Icon, Text, Fill, Stroke, IconImage } from 'ol/style';
+import { Style, Icon, Text, Fill, Circle, IconImage } from 'ol/style';
 import VectorSource from 'ol/source/Vector';
 import { Vector as VectorLayer } from 'ol/layer';
 import { fromLonLat } from 'ol/proj.js';
 import { Feature } from 'ol';
 
-let map;
+let mapa;
 
 const iconStyle = new Style({
-    image: new Icon({
-        anchor: [0.5, 46],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'pixels',
-        src: nuevoazul
-    }),
-    text: new Text({
-        text: 'Bienvenido a Londres',
-        font: 'bold 15px Open Sans',
-        //font: 'bold 20px "Open Sans", "Arial Unicode MS", "sans-serif"',
-        fill: new Fill({
-            color: 'blue'
-        }),
-        offsetX: 100,
-        offsetY: 8
+    image: new Circle({
+        radius: 9,
+        fill: new Fill({ color: 'red' })
     })
 });
 class Maps1 extends React.Component {
 
-    componentWillReceiveProps = (nextprops) => {
+    componentWillReceiveProps = nextprops => {
         //console.log(nextprops.selectedKeys1)
         var array = nextprops.selectedKeys1;
-        array.map(a => {
-            console.log(a['EntidadAgricUrbana.longitud'])
-            console.log(a['EntidadAgricUrbana.latitud'])
-            var iconFeature = new Feature({
-                geometry: new Point(fromLonLat([a['EntidadAgricUrbana.longitud'], a['EntidadAgricUrbana.latitud']])),
-                population: 4000,
-                rainfall: 500
+        var markerss = [];
+        console.log(array.length)
+        if (array.length > 0) {
+            array.map(a => {
+                console.log(a['EntidadAgricUrbana.longitud'])
+                //console.log(a['EntidadAgricUrbana.latitud'])
+                var x = parseInt(a['EntidadAgricUrbana.longitud']);
+                var y = parseInt(a['EntidadAgricUrbana.latitud']);
+                var iconFeature = new Feature({
+                    geometry: new Point(fromLonLat([x, y]))
+                });
+                iconFeature.setStyle(iconStyle);
+                markerss.push(iconFeature)
             });
-
-            iconFeature.setStyle(iconStyle);
-
-            var vectorSource = new VectorSource({
-                features: [iconFeature]
+            var vectorsource = new VectorSource({
+                features: markerss
             });
-
             var vectorLayer = new VectorLayer({
-                source: vectorSource
+                source: vectorsource
             });
 
-            map.addLayer(vectorLayer);
-        });
+            mapa.addLayer(vectorLayer);
+        }
+        console.log('ya salio')
     }
 
     componentDidMount = () => {
-        map = new Map({
+        mapa = new Map({
             target: 'map',
             layers: [
                 new TileLayer({
